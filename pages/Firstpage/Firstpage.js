a9309dc40568dc0a06b8b70cdea5f486f98355fe
QM36 <18791767622@163.com>
2018-04-20 00:01:46 +0800

    完善

--- pages/Firstpage/Firstpage.js
+++ pages/Firstpage/Firstpage.js
@@ -1,1 +1,34 @@
-Page({})
+Page({
+  data:{
+    foodlist: [
+      {
+        id: "1",
+        imagePath: "/image/1.png",
+      },
+      {
+        id: "2",
+        imagePath: "/image/2.png",
+      },
+      {
+        id: "3",
+        imagePath: "/image/3.png",
+      },
+      {
+        id: "4",
+        imagePath: "/image/4.png",
+      },
+      {
+        id: "5",
+        imagePath: "/image/5.png",
+      },
+      {
+        id: "6",
+        imagePath: "/image/6.png",
+      },
+      {
+        id: "7",
+        imagePath: "/image/7.png",
+      }
+    ]
+  }
+})
