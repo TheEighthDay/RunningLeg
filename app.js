49915a80b75c6eae3275e3f1a9bdda6762739b30
1109419614 <1109419614@qq.com>
2018-04-20 22:17:45 +0800

    用户登陆机制完善数据库初步建成

--- app.js
+++ app.js
@@ -1,67 +1,131 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-    // 登录
-    wx.login({
-      success: res => {
-        wx.request({
-          url: 'https://theeighthday.cn/loginn',
-          data:{
-            code:res.code
-          },
-          success:res =>{
-            if (res.statusCode != 200){
-              console.log("没有拿到openid session_key")
-            }
-            else{
-              console.log("成功拿到openid session_key")
-              console.log(res.data)
-            }
-            
-          }
-        })
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-   
+    // wx.removeStorageSync('skey'); //清除缓存
+    var that = this;
+    console.info('登陆 app...');
+    wx.showLoading({
+      title: '登陆中',
+      mask: true
+    });
+    console.info('检查授权ing');
     wx.getSetting({
       success: res => {
-        if (!res.authSetting['userInfo']) {
+        if (res.authSetting['scope.userInfo']) {
+          console.info("已经授权");
+        }
+        else{
           wx.authorize({
             scope: 'scope.userInfo',
             success() {
-              // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
-              console.log("授权ok")
+              console.info("授权成功");
             },
             fail() {
-              console.log("用户不给授权")
-              // do something
+              //弹窗要授权
+              console.info("授权失败");
+              wx.showModal({
+                title: '提示',
+                content: '拒绝授权可能会影响定位功能使用，请删除小程序或设置授权',
+                confirmText: '去设置',
+                success: res => {
+                  if (res.confirm) {
+                    wx.openSetting({
+                    })
+                  }
+                }
+              }) 
+              console.log("重新授权成功") 
             }
           })
-
         }
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              console.log(res.userInfo)
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+      wx.getUserInfo({
+        success: function (res) {
+          that.globalData.userInfo = res.userInfo;
+        }
+      })
+      that.checkLogin();
+      },
+    })
+  },
+  checkLogin:function(){
+    console.info("检查登陆...");
+    var skey = wx.getStorageSync('skey');
+    if(skey){
+      console.info("skey存在,不用登陆或注册，直接获取信息")
+      this.getUserInfo();
+    }
+    else{
+      console.info("skey不存在,要么本地储存被删除，要么新用户，将注册或登陆")
+      this.loginAndRegister();
+    }
+  },
+  
+  loginAndRegister: function () {
+    var that = this;
+    wx.login({
+      success: res => {
+        var userInfo = {
+          name: that.globalData.userInfo.nickName,
+          avatar: that.globalData.userInfo.avatarUrl,
+          code: res.code
+        };
+        // console.log(userInfo)
+        that.request({
+          url: 'https://theeighthday.cn/registeruser', //注册用户
+          method: 'GET',
+          data: userInfo,
+          success: function (res) {
+            if (res.statusCode==200){
+              console.info("注册或者登陆成功");
+              try {
+                wx.setStorageSync('skey', res.data.skey);
+              } catch (e) {
+                console.info("存储skey失败");
               }
+              wx.hideLoading();
             }
-          })
+            else{
+              console.info("注册失败");
+              wx.hideLoading();
+            }
+          },
+          fail : function(res){
+            console.log(res);
+          } 
+        })
+      }
+    })
+  },
+  getUserInfo: function (){
+    var that=this;
+    this.request({
+      url:'https://theeighthday.cn/userinfo',  //获取userinfo
+      success:function(res){
+        //未登录，session过期
+        if (res.data.skey)
+        {
+          console.info("skey过期重新获取");
+          console.log(res.data.skey);
+          wx.setStorageSync('skey', res.data.skey);
+          that.getUserInfo();
+        }
+        else{
+          that.globalData.userInfo = res.data;
+          console.log(that.globalData.userInfo)
+          wx.hideLoading();
         }
       }
     })
+  },
+ 
+  request: function (obj) {
+    var skey = wx.getStorageSync('skey');
+    if(skey){
+      obj.header = {
+        skey:skey,
+      }
+    } 
+    wx.request(obj)
   },
   globalData: {
     userInfo: null
