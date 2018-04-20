ad125fa35570d918886641e98fc9a9c3c800e012
QM36 <18791767622@163.com>
2018-04-20 18:34:19 +0800

    优化第二个页面

--- pages/Detailinfo/Detailinfo.js
+++ pages/Detailinfo/Detailinfo.js
@@ -1,9 +1,9 @@
 Page({
   data:{
-    todolist:
+    detail:
       {
         address:"电子科技大学（沙河校区）15栋505",
-        phone:"13322224444",
+        phonenumber:"13322224444",
         time:"2018/4/19 12：00",
         others:"汤！！！上楼慢一点不要撒了",
       },
--- pages/Secondpage/Secondpage.js
+++ pages/Secondpage/Secondpage.js
@@ -1,1 +1,10 @@
-Page({})
+Page({
+  data: {
+    detail :{
+      address:"电子科大（沙河校区）15栋 505",
+      phonenumber:"13322224444",
+      time:"2018/4/19 12：00",
+      others:"汤多！！！上楼慢一点！",
+    }
+  }
+})
