3f29b8266497e758cc300250178063bba77fac14
2631478675 <2631478675@qq.com>
2018-05-10 17:51:00 +0800

    want to change style

--- pages/PersonalInformation/PersonalInformation.js
+++ pages/PersonalInformation/PersonalInformation.js
@@ -1,67 +1,95 @@
-var app = getApp()
+const app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    userInfo: {}
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
   onLoad: function () {
-  
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+
   },
-  
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  blurName: function (e) {
+    this.setData({ name: wx.getStorageSync('name') });
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  changeName: function (e) {
+    var name = e.detail.value.trim();
+
+    if (name) {
+      wx.setStorageSync('name', name);
+    }
+
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
+  changeAvatar: function (e) {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
+    var that = this;
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
+    wx.chooseImage({
+      success: function (res) {
+        var tempFilePaths = res.tempFilePaths;
+        wx.saveFile({
+          tempFilePath: tempFilePaths[0],
+          success: function (res) {
+            var savedFilePath = res.savedFilePath;
+            wx.setStorageSync('avatar', savedFilePath);
+            that.setData({ avatar: savedFilePath });
+          }
+        });
+      }
+    })
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  kindToggle: function (e) {
+    var id = e.currentTarget.id, histories = this.data.histories;
+    for (var i = 0, len = histories.length; i < len; ++i) {
+      if (histories[i].id == id) {
+        histories[i].open = !histories[i].open
+      } else {
+        histories[i].open = false
+      }
+    }
+    this.setData({
+      histories: histories
+    });
   }
-})
+}
+  )
